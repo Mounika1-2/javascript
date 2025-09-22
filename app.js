be0368a2f3769263d26b2637acdc64c4d2f846cf
Mounika <mounikaofficial15@gmail.com>
2025-09-22 11:02:28 +0530

    datatypes

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 //var is aglobal variable
 //let and const are local variables
 
-var a=67;
-var a=91;
-let b=68;
-console.log(`value of b is ${b}`);
-console.log(b);
-console.log(a);
+// var a=67;
+// var a=91;
+// let b=68;
+// console.log(`value of b is ${b}`);
+// console.log(b);
+// console.log(a);
+//  this is used for multiple line commit
+//types of data types primitive,non primmitiva
+// primitive : Number,String,bool,undefined,null(object)
+// non primitive: Array,object 
+// typeof() to know datatype
+// this,window is command used to add or display javascript functions
+// print command is used to print document
+a= 7367;
+console.log(typeof(a));
+name="Krishna";
+console.log(typeof(name));
+c=true;
+console.log(typeof(c));
+data=null;
+console.log(typeof(data));
+d=undefined;
+console.log(typeof(d));
+
+// non primitive
+array=[56,"Krishna",true,null];
+console.log(type(arrray));
+obj={
+    name: "Krishna",
+    age: "23",
+    dept: "CSE"};
+console.log(typeof(obj));
